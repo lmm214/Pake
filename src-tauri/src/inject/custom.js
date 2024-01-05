96cb0eb7b6afd82f806e2e0cca7314b64fd175d9
林木木 <fivemu@gmail.com>
2024-01-05 15:58:44 +0800

    Update custom.js
    Signed-off-by: 林木木 <fivemu@gmail.com>

--- src-tauri/src/inject/custom.js
+++ src-tauri/src/inject/custom.js
@@ -4,8 +4,10 @@
  * Additionally, you can directly include any script files in this file
  * that you wish to attach to the application.
  */
+window.addEventListener('DOMContentLoaded', _event => {
   const link = document.createElement("link");
   link.rel = "stylesheet";
   link.type = "text/css";
   link.href = "https://npm.elemecdn.com/lxgw-wenkai-screen-webfont/style.css";
   document.head.append(link);
+})
